a869c726a8c77ba70ad3c2d79045bdccc5641240
qwc-ch <zzzzzzxx2022@163.com>
2026-05-03 10:53:09 +0800

    修改友链

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -51,11 +51,11 @@ export const friendsConfig: FriendLink[] = [
 		enabled: true,
 	},
 	{
-	  title: "番茄主理人",
+	  title: "fqzlr",
 		imgurl:
-			"https://q1.qlogo.cn/g?b=qq&nk=20447289&s=640",
-		desc: "坐而言不如起而行.",
-		siteurl: "番茄主理人",
+			"https://q1.qlogo.cn/g ?b=qq&nk=20447289&s=640",
+		desc: "坐姿不如起而行。",
+		siteurl: "https://fqzlr.com/",
 		tags: ["Blog"],
 		weight: 7, // 权重，数字越大排序越靠前
 		enabled: true, // 是否启用
