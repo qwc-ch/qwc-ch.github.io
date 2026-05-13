00653cd5ef2f7cf3c4dd1bff9b2ee27c4725aeac
qwc-ch <zzzzzzxx2022@163.com>
2026-05-13 15:07:12 +0800

    添加友链

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -63,6 +63,16 @@ export const friendsConfig: FriendLink[] = [
 		enabled: true,
 		issue_id: 19,
 	},
+			{
+		title: "UpXuu",
+		imgurl: "https://upxuu.com/images/20260214145619.jpg",
+		desc: "逐光而上",
+		siteurl: "https://upxuu.com/",
+		tags: ["Blog"],
+		weight: 10,
+		enabled: true,
+		issue_id: 20,
+	},
 ];
 
 // 获取启用的友链并进行排序
